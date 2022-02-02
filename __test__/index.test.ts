3c661631524e70c477566d6ad5a24efbcb972d35
chillrod <rodramone@live.com>
2022-02-02 20:11:41 -0300

    boilerplate

---
+++ __test__/index.test.ts
@@ -0,0 +1,9 @@
+import { boilerplate } from "../src/lib/index";
+
+describe("Name of the group", () => {
+  it("should ", () => {
+    const state = boilerplate();
+
+    expect(state).toBe("Boilerplate");
+  });
+});
