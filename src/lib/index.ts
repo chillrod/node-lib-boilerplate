4651af638de11784ea09bbaa24dbcd936eabd8e0
chillrod <rodramone@live.com>
2022-12-05 18:16:14 -0300

    feat: started

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,14 @@
-export const boilerplate = () => {
-  return "Boilerplate";
-};
+import express from "express";
+
+import "reflect-metadata";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "My friend!" });
+});
+
+app.listen(3333, () => {
+  console.log("listening on port 3333");
+});
